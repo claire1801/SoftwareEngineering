7b9a20805f5124c9ba2df3638d0752b7c5ddeeca
jamie <jamie@DESKTOP-O1SKES2.home>
2019-02-04 18:40:04 +0000

    Initial Commit

--- Coursework/src/HelloWorld.java
+++ Coursework/src/HelloWorld.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld {
-public static void main(String[] args) {
-	System.out.println("hello world");
-}//test3
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}//Initial commit
 }
