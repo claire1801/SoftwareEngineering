fbaf2605ff7e7d2f73e1dc645908dcfd07df0bd6
lr53 <46905748+lr53@users.noreply.github.com>
2019-02-05 13:25:57 +0000

    Update MenuList.java

--- Coursework/src/MenuList.java
+++ Coursework/src/MenuList.java
@@ -1,14 +1,47 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MenuList {
+	
+	TreeMap<String, ? extends MenuItem> menuList;
 
-	public void addItem(MenuItem newMenuItem) {
-		// TODO Auto-generated method stub
+	public MenuList(TreeMap<String, ? extends MenuItem> menuList) {
 		
+		this.menuList = menuList;
 	}
 
-	public void writeReport(String string) {
-		// TODO Auto-generated method stub
-		
+	public TreeMap<String, ? extends MenuItem> getMenuList() {
+		return menuList;
 	}
 
+	public MenuItem get(String key) {
+	        return menuList.get(key);
+	}
+	 
+	public MenuItem remove(String key){
+			return menuList.remove(key);
+	}
+
+	public void printAll() {
+		for (Map.Entry<String, ? extends MenuItem> entry : menuList.entrySet()) {
+     	System.out.println("Key: " + entry.getKey() + ". Value: " + entry.getValue());
+		}
+	}
+	
+	public void writeReport() {
+		FileWriter fw;
+
+		try {
+			fw = new FileWriter("MenuReport.txt");
+			fw.write(String.format("%-40s %-5s \n\n", "Menu Item", "Ordered"));
+		
+			fw.close();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	
+	}	
+	
 }
