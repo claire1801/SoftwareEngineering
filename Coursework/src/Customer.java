c8dcc72462f58806083202be9b9ff80d0cb25724
lr53 <46905748+lr53@users.noreply.github.com>
2019-02-05 13:27:45 +0000

    Update Customer.java

--- Coursework/src/Customer.java
+++ Coursework/src/Customer.java
@@ -1,8 +1,39 @@
-
 public class Customer {
-
-	public Customer(String string, int iD, int noDrinks) {
-		// TODO Auto-generated constructor stub
+	
+	private int customerId;
+	private boolean member;
+	private int numberPreviousCoffees;
+	
+	public Customer(int customerID, boolean member, int numberPreviousCoffees) {
+		this.customerId = customerId;
+		this.member = member;
+		this.numberPreviousCoffees = numberPreviousCoffees;
 	}
 
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public boolean isMember() {
+		return member;
+	}
+
+	public void setMember(boolean member) {
+		this.member = member;
+	}
+
+	public int getNumberPreviousCoffees() {
+		return numberPreviousCoffees;
+	}
+
+	public void setNumberPreviousCoffees(int numberPreviousCoffees) {
+		this.numberPreviousCoffees = numberPreviousCoffees;
+	}
+	
+	
+	
 }
