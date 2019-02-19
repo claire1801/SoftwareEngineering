614258bb790b149e1c371f2d8b207053a645ee65
claire1801 <46598248+claire1801@users.noreply.github.com>
2019-02-19 14:15:40 +0000

    Update MenuItems.java

--- Coursework/src/main/MenuItems.java
+++ Coursework/src/main/MenuItems.java
@@ -1,11 +1,28 @@
 package main;
+
+/**
+ * 
+ * @author clairecullen
+ *
+ */
+
 public abstract class MenuItems {
 	
+
 	protected String uniqueID;
 	protected double cost; 
 	protected String description;
 	protected String allergens;
 	protected String name;
+	
+/**
+ * 
+ * @param name of item
+ * @param unique identifier
+ * @param cost
+ * @param description
+ * @param allergens
+ */
 	
 	public MenuItems(String name, String id, double cost, String desc, String algns) {
 		try {
@@ -24,24 +41,47 @@ public abstract class MenuItems {
 	}
 }
 	
+	/**
+	 * 
+	 * @return
+	 */
+	
 
-	public String getName() { return name; }
+	public String getName() 
+	{ 
+		return name; }
 	
-	public String getID() { return uniqueID; }
+	public String getID() 
+	{ 
+		return uniqueID; }
 	
-	public void setID(String newID) { uniqueID = newID; }
+	public void setID(String newID) 
+	{ 
+		uniqueID = newID; }
 	
-	public double getCost() { return cost; }
+	public double getCost() 
+	{ 
+		return cost; }
 	
-	public void setCost(double newCost) { cost = newCost; }
+	public void setCost(double newCost) 
+	{ 
+		cost = newCost; }
 	
-	public String getDescription() { return description; }
+	public String getDescription() 
+	{ 
+		return description; }
 	
-	public void setDescription(String newDescription) { description = newDescription; }
+	public void setDescription(String newDescription) 
+	{ 
+		description = newDescription; }
 	
-	public String getAllergens() { return allergens; }
+	public String getAllergens() 
+	{ 
+		return allergens; }
 	
-	public void setAllergens(String newAllergens) { allergens = newAllergens; }
+	public void setAllergens(String newAllergens) 
+	{ 
+		allergens = newAllergens; }
 	
 	
 
