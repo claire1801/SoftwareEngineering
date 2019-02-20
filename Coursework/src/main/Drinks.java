0c13cf6b6b3949bdc84a0f6d765168a1e27df141
claire1801 <46598248+claire1801@users.noreply.github.com>
2019-02-20 14:23:52 +0000

    Update Drinks.java

--- Coursework/src/main/Drinks.java
+++ Coursework/src/main/Drinks.java
@@ -1,12 +1,13 @@
 package main;
-//package cafe;
+
 
 public class Drinks extends MenuItems {
 
-	public Drinks(String name, String id, double cost, String desc, String algns) {
+	public Drinks(String name, String id, double cost, String desc, String algns) throws InvalidItemIdentifierException  {
 		super(name, id, cost, desc, algns);
 		
 	}
 
 	
 }
+
