84f804a51af26c7db56c0a87f680de8b361446b0
lr53 <46905748+lr53@users.noreply.github.com>
2019-02-21 14:56:36 +0000

    Update OrderListTest.java

--- src/test/OrderListTest.java
+++ src/test/OrderListTest.java
@@ -12,7 +12,7 @@ class OrderListTest {
 
 	Timestamp timestamp = new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
 	
-	Order order = new Order(1, 100, timestamp, "FOOD123", 2.00, 0.20);
+	Order order = new Order(1, 100, timestamp, "FOOD123", 2.00, 0.20, 1);
 	
 	OrderList orderList = new OrderList();
 	
@@ -30,7 +30,7 @@ class OrderListTest {
 
 	@Test
 	void testRemoveOrder() {
-		Order order2 = new Order(0002, 101, timestamp, "DRINK123", 3.0, 0.2);
+		Order order2 = new Order(0002, 101, timestamp, "DRINK123", 3.0, 0.2, 2);
 		orderList.removeOrder(order2);
 		assertFalse(orderList.getOrderList().contains(order2));
 	}
@@ -49,8 +49,8 @@ class OrderListTest {
 	void testWriteReport(){
 		String tstamp1 = timestamp.toString();
 		String report = orderList.writeReport();
-		report = report.substring(0, 45);
-		assertEquals("1/100/" + tstamp1 + "/FOOD123/2.0/0.2", report);
+		report = report.substring(0, 49);
+		assertEquals("1/100/" + tstamp1 + "/FOOD123/2.00/0.20/1", report);
 	}
 
 }
