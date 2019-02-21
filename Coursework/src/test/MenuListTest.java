1b3c8cfd6a0caed04ce228c88507d7d556b79013
frsrg <46921969+frsrg@users.noreply.github.com>
2019-02-21 13:50:29 +0000

    merging tests to the directory

--- Coursework/src/test/MenuListTest.java
+++ Coursework/src/test/MenuListTest.java
@@ -14,25 +14,26 @@ class MenuListTest {
 
 	@BeforeAll
 	static void setUpBeforeClass_testsConstructor() throws Exception {
-		TreeMap<String, MenuItems> menu_input = new TreeMap<>();
+		menuList = new MenuList();
+		//TreeMap<String, MenuItems> menu_input = new TreeMap<>();
 		String key = "drinkID";
 		Drinks drink = new Drinks("nameDrink",key,1.0,"descriptionDrink","alergensDrink");
 		//Snacks snack = new Snacks("nameSnack","idSnack",2.0,"descriptionSnack","alergensSnack");
 		//Meals meal = new Meals("nameMeal","idMeal",3.0,"descriptionMeal","alergensMeal");
-		menu_input.put(key, drink);
-		menuList = new MenuList(menu_input);
+		menuList.addItem(key, drink);
+		//menuList = new MenuList(menu_input);
 	}
 
 
-	// I don't really understand the point/need of this method
-	@Test
-	void testGetMenuList_works() {
-		fail();
-	}
+//	// I don't really understand the point/need of this method
+//	@Test
+//	void testGetMenuList_works() {
+//		fail();
+//	}
 
 
 	@Test
-	void testAddItem_Exception() {
+	void testAddItem_Exception() throws InvalidItemIdentifierException {
 		String key = "drinkID";
 		Drinks differentDrink = new Drinks("sameDrinkReally",key,1.0,"descriptionDrink","alergensDrink");
 		try {
@@ -45,7 +46,7 @@ class MenuListTest {
 
 	
 	@Test
-	void testGetItem() {
+	void testGetItem() throws InvalidItemIdentifierException {
 		String test_key = "drinkIDtest";
 		Drinks drinkTest = new Drinks("test",test_key,1.0,"test","test");
     	menuList.addItem(test_key, drinkTest);
@@ -97,12 +98,11 @@ class MenuListTest {
 
 	
 	@Test
-	void testWriteReport() {
+	void testWriteReport() throws InvalidItemIdentifierException {
 		String key = "drinkID";
 		Drinks drink = new Drinks("nameDrink",key,1.0,"descriptionDrink","alergensDrink");
-		TreeMap<String, MenuItems> input = new TreeMap<>();
-		input.put(key, drink);
-		MenuList menu = new MenuList(input);
+		MenuList menu = new MenuList();
+		menu.addItem(key, drink);		
 		System.out.println(menu.writeReport());
 		assertEquals(menu.writeReport(),"nameDrink/drinkID/1.0/descriptionDrink/alergensDrink");
 	}
--- Coursework/src/test/TestCustomer.java
+++ Coursework/src/test/TestCustomer.java
@@ -1,39 +1,27 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import main.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Before;
-import org.junit.Test;
 
-import main.Customer;
-//import main.CustomerList;
-import main.MembershipType;
 
-import static org.junit.Assert.*;
-
-import java.util.Hashtable;
-
-public class TestCustomer {
-
+class TestCustomer {
 	private Customer newCustomer;
+	@BeforeEach 
+	public void setup() throws InvalidCustomerIDException {
+		 newCustomer = new Customer(1,MembershipType.MEMBER,1,"sam");
 
-    // method will be called before EVERY test.
-    // ensures we have a new B for every test
-    @Before
-    public void setUp() throws Exception {
-    	Hashtable<Integer, Customer> list = new Hashtable<>();
-    	
-    	 newCustomer = new Customer(1,MembershipType.MEMBER,1,"sam");
-
-    }
-
-    @Test
+	}
+	@Test
     public void test_getID() {
         // the message (1st parameter is optional)
     	
     	assertEquals(1,newCustomer.getCustomerId());
     }
     @Test
-    public void test_set_id() {
+    public void test_set_id() throws InvalidCustomerIDException {
         // the message (1st parameter is optional)
     	newCustomer.setCustomerId(2);
     	assertEquals(2,newCustomer.getCustomerId());
@@ -50,8 +38,17 @@ public class TestCustomer {
     	newCustomer.setNumberPreviousCoffees(4);
     	assertEquals(4,newCustomer.getNumberPreviousCoffees());
     }
+    @Test
+    public void test_getandsetname() {
+        // the message (1st parameter is optional)
+    	newCustomer.setName("sam2");
+    	assertEquals("sam2",newCustomer.getName());
+    }
+    @Test
+    public void test_SetandGetMember() {
+        // the message (1st parameter is optional)
+    	newCustomer.setType(MembershipType.STUDENT);
+    	assertEquals(MembershipType.STUDENT,newCustomer.getType());
+    }
 
-
-
- 
 }
